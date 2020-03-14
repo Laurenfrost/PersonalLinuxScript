a295683d1dd8c7ccf87feb2673487b5ea2a7b416
Laurenfrost <bellfahle@outlook.com>
2020-03-14 19:08:54 +0800

    add pkg-config & OpenGL & Qt5 support

--- OpenCV-4.1.0-build.sh
+++ OpenCV-4.1.0-build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+##build ffmpeg before this script## 
 
-#apt install cmake libgtk2.0-dev pkg-config
+##gtk2.0 should be installed for namedwindow.
+#apt install cmake libgtk2.0-dev pkg-config python python-numpy python3-dev python-scipy
+
+##"Qt5_DIR" lead to where Qt5 installed.
+#export Qt5_DIR=/opt/Qt5.1.1/5.1.1/gcc_64
 
 rm -rf build
 mkdir build
@@ -24,6 +29,9 @@ cmake \
     -DWITH_GSTREAMER=OFF \
     -DWITH_GSTREAMER_0_10=OFF \
     -DWITH_CUDA=OFF \
+#   -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda \
+    -DWITH_QT=ON \
+    -DWITH_OPENGL=ON \
     -DWITH_GTK=ON \
     -DWITH_VTK=OFF \
     -DWITH_TBB=OFF \
@@ -33,5 +41,6 @@ cmake \
     -DINSTALL_PYTHON_EXAMPLES=ON \
     -DINSTALL_TESTS=OFF \
     -DOPENCV_EXTRA_MODULES_PATH=/home/dedicatus/Downloads/opencv_contrib-4.2.0/modules \
+#turn "OPENCV_GENERATE_PKGCONFIG" on to generate opencv.pc
     -DOPENCV_GENERATE_PKGCONFIG=ON \
     ..
